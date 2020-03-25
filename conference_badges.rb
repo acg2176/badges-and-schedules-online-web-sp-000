48efb72ff4f7612c31d0ed4b8774aa6e54ea1368
Anna Guerrero <acg2176@columbia.edu>
2020-03-25 00:09:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,12 @@ def badge_maker(name)
   puts "Hello, my name is #{name}"
 
 end
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |attendee|
+    badge_message = badge_maker(attendee)
+    new_array.append(badge_message)
+  end
+  return new_array
+end
