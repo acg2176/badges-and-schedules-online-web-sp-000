19d2ba6cb1965cd796d1462b02cf58c936981475
Anna Guerrero <acg2176@columbia.edu>
2020-03-25 00:45:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,8 +15,9 @@ end
 
 def assign_rooms(speakers)
   new_array = []
-  speakers.each do |speaker|
-    room_assign = "Hello, #{speaker}! You'll be assigned to room 1!"
+  speakers.each_with_index do |speaker, index|
+
+    room_assign = "Hello, #{speaker}! You'll be assigned to room #{index}!"
     new_array.append(room_assign)
   end
   return new_array
